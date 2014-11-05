02d75366bb8e544c25d8e49d4488edfb6dd3cdda
unknown <gzs3049@GIH-D-6399.yajuenet.internal>
2014-11-05 13:38:04 +0800

    finish load

--- SimpleParser.py
+++ SimpleParser.py
@@ -1,5 +1,6 @@
 __author__ = 'gzs3049'
 # coding=utf-8
+# from copy import deepcopy
 
 
 class SimpleParser:
@@ -7,21 +8,23 @@ class SimpleParser:
         self.pDict = dict()
 
     def load(self, s):
-        global objdict
         index = 0
-        if s[index] == '{':
-            objdict, index = self.getobject(index + 1, s)
-            assert objdict is not None
-            for key, value in objdict:
-                self.pDict[key] = value
-        # elif s[index] == '[':
-        # objlist, index = self.getlist(index + 1, s)
-        # elif s[index] == '"':
-        # string, index = self.getstring(index + 1, s)
-        elif s[index] == " ":
-            index += 1
-        elif index >= len(s):
-            raise Exception("Out of Bound")
+        length = len(s)
+        while 1:
+            if index == length:
+                raise SyntaxError("Invalid string {}".format(s))
+            elif s[index] == " ":
+                index += 1
+            elif s[index] == '{':
+                objdict, index = self.getobject(index + 1, s)
+                assert objdict is not None
+                self.pDict = objdict
+                if index == length:  # 由于假设最外层是object，读完一个object后如果还剩下字符（包含空字符）都将报错
+                    return
+                else:
+                    raise SyntaxError("Invalid json string")
+            else:
+                index += 1
 
     def getobject(self, index, string):
         global haskey, idx, key, value
@@ -46,8 +49,8 @@ class SimpleParser:
             elif string[idx] == '[':
                 value, idx = self.getlist(idx + 1, string)
                 objdict[key] = value
-            elif "1" < string[idx] < "9":
-                value, idx = self.getnumber(self, idx, string)
+            elif "1" <= string[idx] <= "9":
+                value, idx = self.getnumber(idx, string)
                 objdict[key] = value
             elif string[idx] == 't':
                 if idx + 4 < length and string[idx:idx + 4] == 'true':
@@ -55,7 +58,7 @@ class SimpleParser:
                     idx += 4
                     objdict[key] = value
                 raise SyntaxError("Invalid Symbole {}".format(string[idx:idx + 4]))
-            elif string[idx + 1] == 'f':
+            elif string[idx] == 'f':
                 if idx + 5 < length and string[idx:idx + 5] == 'false':
                     value = False
                     idx += 5
@@ -77,7 +80,7 @@ class SimpleParser:
             elif idx == length:
                 raise SyntaxError("Out of length")
             else:
-                raise Exception("Exception happen in symbol{} in index {}".format(string[idx], idx))
+                raise Exception("Exception happen in symbol {} in index {}".format(string[idx], idx))
 
     def getlist(self, index, string):
         global idx, obj
@@ -103,7 +106,7 @@ class SimpleParser:
             elif string[idx] == ']':
                 return objlist, idx + 1
             elif "1" <= string[idx] <= "9":
-                obj, idx = self.getnumber(self, idx, string)
+                obj, idx = self.getnumber(idx, string)
                 objlist.append(obj)
             elif string[idx] == 't':
                 if idx + 4 < length and string[idx:idx + 4] == 'true':
@@ -134,8 +137,40 @@ class SimpleParser:
     def getnumber(cls, nindex, string):
         global idx
         idx = nindex
-        number = 0
-        return number, idx
+        isfloat = False  # 是否是浮点数
+        isint = True  # 是否是整数
+        haspower = False  # 是否含有幂次
+        length = len(string)
+        while 1:
+            if idx == length or string[idx] == '}' or string[idx] == ']' or string[idx] == ',':
+                if isfloat or haspower:
+                    return float(string[nindex:idx]), idx
+                elif isint:
+                    return int(string[nindex:idx]), idx
+            elif string[idx] == '.':
+                if isfloat:
+                    raise ValueError("Invalid symbol: {} appear for twice".format(string[idx]))
+                else:
+                    isfloat = True
+                    idx += 1
+            elif string[idx] == 'e' or string[idx] == 'E':
+                if haspower:
+                    raise ValueError("Invalid symbol: {} appear for twice".format(string[idx]))
+                else:
+                    haspower = True
+                    idx += 1
+            elif string[idx] == '+' or string[idx] == '-':
+                if idx == nindex or string[idx - 1] == 'e' or string[idx - 1] == 'E':
+                    idx += 1
+                    if idx == length:
+                        raise ValueError("Invalid string {}".format(string[nindex:idx], idx))
+                else:
+                    raise ValueError("Invalid symbol:{}".format(string[idx - 1:idx + 1]))
+            elif "0" <= string[idx] <= "9":
+                idx += 1
+                continue
+            else:
+                raise ValueError("Invalid symbol: {}".format(string[idx]))
 
     @classmethod
     def getstring(cls, sindex, string):
--- SimpleParserTest.py
+++ SimpleParserTest.py
@@ -23,32 +23,48 @@ class MyTestCase(unittest.TestCase):
             self.assertRaises(SyntaxError, self.sp.getlist, 1, obj)
 
     def test_object(self):
-        objlist = ['{"k":"v"}',
+        objlist = ['{}',
+                   '{"k":"v"}',
                    '{"k":"v","k1":"v"}',
                    '{"k":"v","k1":{"k1":{"k1":"v"}}}',
                    '{"k":[true, false, null]}',
-                   '{"k":     null   }']
+                   '{"k":     null   }',
+                   '{"k":1234, "kv":234}']
         for obj in objlist:
             robj, idx = self.sp.getobject(1, obj)
             self.assertIsNotNone(robj)
-            # print robj
+            print robj
 
     def test_number(self):
-        numbers = ['12345', '-12345', '-123e10', '-123E10', '-123E-10', '10.1', '10.1e20']
+        numbers = ['12345', '-12345', '-123e10', '-123E10', '-123E-10', '10.1', '10.1e20]']
         for number in numbers:
             num, idx = self.sp.getnumber(0, number)
+            print "Pair: {} : {}".format(number, num)
             self.assertIsNotNone(num)
-            print "Pair: {} : {}".format(number, num)
 
-    def test_innerconver(self):
-        numbers = ['12345', '-12345', '-123e10', '-123E10', '-123E-10', '10.1', '10.1e20']
-        for number in numbers:
-            num = float(number)
-            print "Pair: {} : {}".format(number, num)
+        invalidnumbers = ['+', '-', '-1.1.1', '++033', '+333+', '0.e', 'e']
+        for number in invalidnumbers:
+            print number
+            self.assertRaises(ValueError, self.sp.getnumber, 0, number)
 
-    def test_innerjson(self):
-        numbers = '[12345, -12345, -123e10, -123E100, -123E-10, 10.1, 10.1e20]'
-        print json.loads(numbers)
+            # def test_innerconver(self):
+            # numbers = ['12345', '-12345', '-123e10', '-123E10', '-123E-10', '10.1', '10.1e20', '-123', '+12e+123']
+            # for number in numbers:
+            # num = float(number)
+            # print "Pair: {} : {}".format(number, num)
+            # #
+            # def test_innerjson(self):
+            # print 1234E12
+            # numbers = '[12345, -12345, -123e10, -123E100, -123E-10, 10.1, 10.1e20]'
+            # print json.loads(numbers)
+
+    def test_load(self):
+        objs = ['{"k":"v","k1":{"k1":{"k1":"v"}}, "k1":{"k1":{"k1":[123,123,false,true,  null]}}}']
+        for obj in objs:
+            self.sp.load(obj)
+            self.assertIsNotNone(self.sp.pDict, "Load Failed!")
+            print self.sp.pDict
+
 
 if __name__ == '__main__':
     unittest.main()
